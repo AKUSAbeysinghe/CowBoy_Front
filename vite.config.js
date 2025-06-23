8b7b28e90b025b21eb58b30d0acfd76fee05d61b
A.K.U.S Abeysinghe <umeshsewwandi420@gmail.com>
2025-06-24 00:31:43 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/your-repo-name/',  // 👈 important! (match your repo name)
+// });
+
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// REPLACE `your-repo-name` with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
+  base: "/CowBoy_Front/",
 })
+
+
+
+
+
+
